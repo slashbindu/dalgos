abbdb1591d3fa9f1e07c0846e8403dd8840a66f7
slashbin <17829390+slashbindu@users.noreply.github.com>
2022-02-02 00:20:43 -0500

    Create bloomfilter.cpp

---
+++ bloomfilter.cpp
@@ -0,0 +1,6 @@
+/* Implement a probablistic datastructure to check if an key existes in massica scale  datastore 
+A simple and lightweight implementation of a Bloom filter.
+   
+   Hashing algorithm used	MD5
+   Expected rate of false positives  ?
+*/
